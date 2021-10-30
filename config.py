b68c2a765b848a1fa7349288a9bb47533ddc6668
Mustache Beard <87588953+Kingache@users.noreply.github.com>
2021-10-30 11:55:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,21 +13,21 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Veez Music")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8628c642a266a22effd8c.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
-CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/0f6f8a8a5ad69fe5ecf3d.png")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/5dd3d6d64ccd785ae5af2.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/84f09e57f97f6e1bb3cba.jpg")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/84f09e57f97f6e1bb3cba.jpg")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/5dd3d6d64ccd785ae5af2.jpg")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezassistant1")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
+BOT_USERNAME = getenv("BOT_USERNAME", "Eagle_Xrobot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "EagleAssistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Mutualan_Cari_Teman")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "infobotrelax")
 # isi dengan username kamu tanpa simbol @
-OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
+OWNER_NAME = getenv("OWNER_NAME", "Manusiabajingann")
 # fill with your nickname
-ALIVE_NAME = getenv("ALIVE_NAME", "Levina")
+ALIVE_NAME = getenv("ALIVE_NAME", "ᴇᴀɢʟᴇ")
 # fill with your id as the owner of the bot
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
@@ -35,7 +35,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 # just fill with True or False (optional)
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 # UPDATER CONFIG
@@ -43,6 +43,6 @@ U_BRANCH = "main"
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_REPO = os.environ.get(
-    "UPSTREAM_REPO", "https://github.com/levina-lab/VeezMusic"
+    "UPSTREAM_REPO", "https://t.me/ikichannellll"
 )
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
